a9f9efd96cc962ab110251c88959657278dcf926
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-06-09 16:54:04 -0500

    3rd commit: Reference Data Types

--- learning-java/src/com/amigoscode/Main.java
+++ learning-java/src/com/amigoscode/Main.java
@@ -1,40 +1,20 @@
 package com.amigoscode;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub (write code here)
 		
-		System.out.println("I'm back to using Java, I am very excited.");
+		String name = new String("Amigoscode");
+		System.out.println(name.toUpperCase());
 		
-		int number = 1;
-		String brand = "Migos";
-		System.out.println();
-		//Date date = new Date();
+		LocalDate now = LocalDate.now();
+		System.out.println(now.getYear());
 		
-		//Primitive data types
-		
-		byte theByte = -128;
-		short theShort = 32000;
-		int num1 = 1;
-		long long1 = 78783833432L;
-		float pi = 3.14F;
-		double doublePi = 3.1415;
-		boolean isAdult = true;
-		char initial = 'A';
-		
-		
-		System.out.println(theByte);
-		System.out.println(theShort);
-		System.out.println(num1);
-		System.out.println(long1);
-		System.out.println(pi);
-		System.out.println(doublePi);
-		System.out.println(isAdult);
-		System.out.println(initial);
-		
-		
-
 	}
 
 }
