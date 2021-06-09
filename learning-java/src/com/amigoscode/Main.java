3fb78e148ea6ff951ca7fe42a3c6cf1c3f201abf
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-06-09 16:14:35 -0500

    2nd commit: Data Primitives

--- learning-java/src/com/amigoscode/Main.java
+++ learning-java/src/com/amigoscode/Main.java
@@ -6,6 +6,34 @@ public class Main {
 		// TODO Auto-generated method stub (write code here)
 		
 		System.out.println("I'm back to using Java, I am very excited.");
+		
+		int number = 1;
+		String brand = "Migos";
+		System.out.println();
+		//Date date = new Date();
+		
+		//Primitive data types
+		
+		byte theByte = -128;
+		short theShort = 32000;
+		int num1 = 1;
+		long long1 = 78783833432L;
+		float pi = 3.14F;
+		double doublePi = 3.1415;
+		boolean isAdult = true;
+		char initial = 'A';
+		
+		
+		System.out.println(theByte);
+		System.out.println(theShort);
+		System.out.println(num1);
+		System.out.println(long1);
+		System.out.println(pi);
+		System.out.println(doublePi);
+		System.out.println(isAdult);
+		System.out.println(initial);
+		
+		
 
 	}
 
