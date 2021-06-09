4714cab959a71c605daef4ddb1e14b3d0b26093a
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-06-09 15:09:46 -0500

    1st commit: 1st JAVA code after about a 2 years

---
+++ learning-java/src/com/amigoscode/Main.java
@@ -0,0 +1,12 @@
+package com.amigoscode;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub (write code here)
+		
+		System.out.println("I'm back to using Java, I am very excited.");
+
+	}
+
+}
