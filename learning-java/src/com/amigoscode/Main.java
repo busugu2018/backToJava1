34c16875e2bd555e4d243b112a95308fbb7a6ad0
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-06-10 09:28:31 -0500

    5th commit: Naming Variables

--- learning-java/src/com/amigoscode/Main.java
+++ learning-java/src/com/amigoscode/Main.java
@@ -8,33 +8,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub (write code here)
+			
+		int zero = 0;
+		int one = 1;
+		double pi = 2.1415;
+		int subscribersCount = 1_000_000;
 		
-		int a = 10;
-		int b = a;
-		a = 100;
-		
-		System.out.println("a = " + a + " - b = " + b);
-		
-		Person alex = new Person ("alex");
-		Person mariam = alex;
-		
-		System.out.println("Before changing alex");
-		System.out.println(alex.name + " " + mariam.name);
-		
-		alex.name = "alexander";
-		
-		System.out.println("After changing alex");
-		System.out.println(alex.name + " " + mariam.name);		
-		
-		System.out.println("No Worries");
-	}
-	
-	static class Person {
-		String name;
-		Person(String name){
-			this.name = name;
-	//Impo
-		}
 	} 
 
 }
